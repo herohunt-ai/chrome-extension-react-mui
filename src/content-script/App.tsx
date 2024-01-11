8eb6d0bc476db70e1cf0d984824d8fa0148899fc
Norbert Niziołek <norbert@niziolek.dev>
2024-01-11 19:13:24 +0100

    Add ThemeProvider to content script

--- src/content-script/App.tsx
+++ src/content-script/App.tsx
@@ -1,17 +1,27 @@
-import { Paper, SvgIcon, Typography } from "@mui/material";
+import {
+  CssBaseline,
+  Paper,
+  SvgIcon,
+  ThemeProvider,
+  Typography,
+} from "@mui/material";
 import ChromeIcon from "../../static/chrome.svg";
+import theme from "../theme";
 
 function App() {
   return (
-    <Paper sx={{ padding: 2 }}>
-      <Typography>Hello World from content script!</Typography>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Paper sx={{ padding: 2 }}>
+        <Typography>Hello World from content script!</Typography>
 
-      {/* Example of using SVG files as components */}
-      <SvgIcon
-        component={ChromeIcon}
-        sx={{ fill: "none", color: "primary.main" }}
-      />
-    </Paper>
+        {/* Example of using SVG files as components */}
+        <SvgIcon
+          component={ChromeIcon}
+          sx={{ fill: "none", color: "primary.main" }}
+        />
+      </Paper>
+    </ThemeProvider>
   );
 }
 
