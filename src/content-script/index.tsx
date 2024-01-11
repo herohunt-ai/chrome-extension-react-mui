c214bbdaa7cad08c575b76f7edef024da2c70516
Norbert Niziołek <norbert@niziolek.dev>
2024-01-11 19:17:05 +0100

    Inject font in content script

--- src/content-script/index.tsx
+++ src/content-script/index.tsx
@@ -1,6 +1,14 @@
 import ReactDOM from "react-dom/client";
 import App from "./App";
 
+function injectFont() {
+  // Example of injecting a font in the content script
+  const style = document.createElement("style");
+  style.innerHTML =
+    "@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap')";
+  document.head.appendChild(style);
+}
+
 function injectReact() {
   const root = document.createElement("div");
 
@@ -17,4 +25,5 @@ function injectReact() {
   reactRoot.render(<App />);
 }
 
+injectFont();
 injectReact();
